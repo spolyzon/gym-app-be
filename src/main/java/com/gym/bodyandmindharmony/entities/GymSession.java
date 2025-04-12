4b8c65fa45e76ead7fbc7d7026bf474c6510c0e8
Spyros Polyzonis <spolyzonis@gmail.com>
2025-04-12 19:50:29 +0300

    Minor refactoring

--- src/main/java/com/gym/bodyandmindharmony/entities/GymSession.java
+++ src/main/java/com/gym/bodyandmindharmony/entities/GymSession.java
@@ -17,7 +17,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
