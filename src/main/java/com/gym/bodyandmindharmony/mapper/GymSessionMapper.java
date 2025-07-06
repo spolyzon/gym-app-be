1b7c3eb1738751bed47c7992c85cf12ca30ce265
Spyros Polyzonis <spolyzonis@gmail.com>
2025-07-06 12:34:46 +0300

    Deleting unused classes

--- src/main/java/com/gym/bodyandmindharmony/mapper/GymSessionMapper.java
+++
@@ -1,9 +0,0 @@
-package com.gym.bodyandmindharmony.mapper;
-
-import com.gym.bodyandmindharmony.entities.GymSession;
-import com.gym.bodyandmindharmony.models.GymSessionModel;
-
-public interface GymSessionMapper {
-
-    GymSessionModel mapToModel(GymSession entity);
-}
--- src/main/java/com/gym/bodyandmindharmony/repositories/GymSessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gym.bodyandmindharmony.repositories;
-
-import com.gym.bodyandmindharmony.entities.GymSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GymSessionRepository extends JpaRepository<GymSession, String> { }
